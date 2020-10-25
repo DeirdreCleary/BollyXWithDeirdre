34bc615044394fcecf72d7ff0896bc02734aa909
Deirdre Cleary <deirdremariecleary@gmail.com>
2020-10-25 15:53:58 +0000

    Adding class details

---
+++ src/components/NextClass.vue
@@ -0,0 +1,51 @@
+<template>
+  <section class="section">
+    <div class="container">
+      <div class="card is-horizontal mgn">
+        <div class="card-content">
+          <div class="columns">
+            <div class="column is-two-fifths">
+              <p class="title">
+                <strong>Join my next class!</strong>
+              </p>
+              <p class="subtitle is-5">
+                Our next class is this Wednesday at 8pm on Zoom.
+              </p>
+              <p class="subtitle is-6">
+                See you then!
+              </p>
+              <a
+                href="https://us02web.zoom.us/j/85401207030?pwd=enJhVmZKZzQ5ckt6MWFUd080d2JuUT09"
+                class="button is-large is-primary"
+                id="learn"
+                >Join class</a
+              >
+            </div>
+            <div class="card-image column is-three-fifths">
+              <figure class="image">
+                <img
+                  class="i"
+                  src="../assets/ClassAnnouncement.png"
+                  alt="Class details"
+                />
+              </figure>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "NextClass",
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="sass" scoped>
+@import '../mq'
+.mgn
+  margin: 2em
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,21 +1,27 @@
 <template>
   <div class="home">
     <Hero />
+    <NextClass />
     <Spotify />
   </div>
 </template>
 
 <script>
 import Hero from "../components/Hero";
+import NextClass from "../components/NextClass";
 import Spotify from "../components/Spotify";
 
 export default {
   name: "home",
-  components: { Hero, Spotify },
+  components: { Hero, Spotify, NextClass },
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="sass" scoped>
 @import '../mq'
+
+.i
+  max-width: 600px
+  max-height: 600px
 </style>
