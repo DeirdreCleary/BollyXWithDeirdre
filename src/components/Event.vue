6f11c7f22e7cfb999d611006e40849ea5f2e7073
Deirdre Cleary <deirdremariecleary@gmail.com>
2020-12-30 16:44:24 +0000

    Allow paypal contributions

--- src/components/Event.vue
+++ src/components/Event.vue
@@ -67,6 +67,33 @@
                   <span class="recurring">{{ recurring }}</span>
                 </div>
               </div>
+              <br />
+              <form
+                action="https://www.paypal.com/donate"
+                method="post"
+                target="_top"
+              >
+                <input
+                  type="hidden"
+                  name="hosted_button_id"
+                  value="TJS4PW896S556"
+                />
+                <input
+                  type="image"
+                  src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif"
+                  border="0"
+                  name="submit"
+                  title="PayPal - The safer, easier way to pay online!"
+                  alt="Donate with PayPal button"
+                />
+                <img
+                  alt=""
+                  border="0"
+                  src="https://www.paypal.com/en_IE/i/scr/pixel.gif"
+                  width="1"
+                  height="1"
+                />
+              </form>
             </div>
             <div class="card-image column is-half">
               <figure class="image">
